1e5b4b0f579ce4bc0a852cf57d952670ef27540d
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-04-15 12:46:18 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function tsp_hk(distance_matrix) {
     var numberOfCities = distance_matrix.length;
 
-    if (numberOfCities === 0 || distance_matrix[0].length === 0) {
+    if (numberOfCities == 0 || distance_matrix[0].length == 0) {
         return 0;
     }
 
