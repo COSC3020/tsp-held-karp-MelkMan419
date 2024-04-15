84d79d379860530ea4aa5d3e532fb19fe9950936
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-04-15 12:49:16 -0600

    Update code.js

--- code.js
+++ code.js
@@ -10,7 +10,7 @@ function tsp_hk(distance_matrix) {
     var cache = {};
 
     for (var startCity = 0; startCity < NUMCities; startCity++) {
-        var currentTourLength = tsp_hk_rec(Array.from(Array(numberOfCities).keys()).filter(city => city !== startCity), startCity);
+        var currentTourLength = tsp_hk_rec(Array.from(Array(NUMCities).keys()).filter(city => city !== startCity), startCity);
         shortestLengthTour = Math.min(shortestLengthTour, currentTourLength);
     }
 
